dda17f1ce25010b5110daa776b238e5b4f743bad
d0ge01 <not.d0ge@anche.no>
2013-08-20 17:14:42 +0200

    Added some derp.

--- what.py
+++ what.py
@@ -3,6 +3,7 @@ name = input("Ciao puoi inserire il tuo nome: ")
 
 classe = ""
 condizione = 1
+alfabeto = ['a', 'b', 'c', 'd', 'e']
 
 while condizione:
 	print("Che classe frequenti ? \n a) Prima \n b) Seconda \n c) Terza \n d) Quarta \n e) Quinta\n Scegli: ")
@@ -19,9 +20,16 @@ while condizione:
 print("Quale corso segui?\n a) Sistemi informativi aziendali\n b) Turismo\n c) Operatore socio sanitario")
 condizione = 1
 while condizione:
-	sezione = input()
+	corso = input()
 	condizione = not(re.match( r'[abc]', sezione, re.M|re.I ))
-	
+
+classe = alfabeto.index(classe.lower())
+corso = corso.lower()
+
+corso = corso.replace('a','Sistemi informativi aziendali')
+corso = corso.replace('b','Turismo')
+corso = corso.replace('c','Operatore socio sanitario')
+
 print("ciao ", name, " che frequenta la ", classe, " sezione ", sezione, " e vuoi fare ", corso, "\n")
 
 input()
