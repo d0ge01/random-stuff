a412dd020bb5c6a51826a0fcabbdaa2ff8e97c6e
d0ge01 <not.d0ge@anche.no>
2013-09-30 18:23:53 +0200

    30/09 (1)

--- Esercizi_informatica/src/it/d0ge01/exercises/ex3009/Ex3009.java
+++ Esercizi_informatica/src/it/d0ge01/exercises/ex3009/Ex3009.java
@@ -15,5 +15,51 @@ public class Ex3009 extends ExTemplate {
 		"scrivi numero da vettore di interi","Conta elementi distinti in un vettore",
 		"Verifica se in un vettore di interi c'è uno 0","ricerca binaria su vettore di caratteri :D"};
 	private static int sizeV = 3;
+	
+	public void esercizio1() throws IOException {
+		char[] v1 = new char[sizeV];
+		char[] v2 = new char[sizeV];
+		char[] v3 = new char[sizeV];
+		
+		Util.loadArrayChar("Inserisci un carattere (1v)", v1);
+		Util.loadArrayChar("Inserisci un carattere (2v)", v2);
+		Util.loadArrayChar("Inserisci un carattere (3v)", v3);
+		
+		System.out.println("Il vettore uno: " + checkOnlyN(v1));
+		System.out.println("Il vettore due: " + checkOnlyN(v2));
+		System.out.println("Il vettore tre: " + checkOnlyN(v3));
+	}
+	
+	private boolean checkOnlyN(char[] v) {
+		boolean found = true;
+		
+		for ( int i = 0 ; i < v.length && found; i++ )
+			if ( ( v[i] < '0') || ( v[i] > '9'))
+				found = false;
+		
+		return found;
+	}
+	
+	public void esercizio2() throws IOException {
+		double[] v = new double[sizeV];
+		Util.loadArrayDouble("Inserisci un reale: ", v);
+		double p = 1.0;
+		
+		
+		for ( int i = 0 ; i < v.length ; i++ )
+			p *= v[i];
+		
+		System.out.println("Il prodotto e' : " + p);
+	}
 
+	public void esercizio3() throws IOException {
+		int[] v = new int[sizeV];
+		
+		Util.loadArrayInt("Inserisci un naturale tra 0 e 9: ", v);
+		
+		for ( int i = 0 ; i < v.length ; i++ )
+			if ( ( v[i] >= 0 ) && (v[i] <= 9))
+				System.out.print(v[i]);
+		System.out.println("");
+	}
 }
