a2c146cfaab52c42413af91069e7145cc50dbb18
d0ge01 <not.d0ge@anche.no>
2013-09-30 18:12:41 +0200

    some fixes

--- Esercizi_informatica/src/it/d0ge01/exercises/Main.java
+++ Esercizi_informatica/src/it/d0ge01/exercises/Main.java
@@ -3,6 +3,7 @@ package it.d0ge01.exercises;
 import java.io.IOException;
 
 import it.d0ge01.exercises.ex2309.*;
+import it.d0ge01.exercises.ex3009.*;
 import it.d0ge01.exercises.util.*;
 
 public class Main {
@@ -13,7 +14,8 @@ public class Main {
 	private static void menu() throws IOException {
 		int sc = 0;
 		while ( true ) {
-			System.out.println("1  - Esercizi 23 / 09 ");
+			System.out.println("1- Esercizi 23 / 09 ");
+			System.out.println("2- Esercizi 30 / 09 ");
 			System.out.println("99 - Crediti ");
 			System.out.println("0  - Esci ");
 			sc = Util.inputInt();
@@ -21,6 +23,8 @@ public class Main {
 			if ( sc == 1 )
 				new Ex2309();
 			
+			if ( sc == 2 )
+				new Ex3009();
 			if ( sc == 99 )
 				credits();
 			
--- Esercizi_informatica/src/it/d0ge01/exercises/ex3009/Ex3009.java
+++ Esercizi_informatica/src/it/d0ge01/exercises/ex3009/Ex3009.java
@@ -6,9 +6,14 @@ import it.d0ge01.exercises.util.*;
 
 public class Ex3009 extends ExTemplate {
 
-	public Ex3009(String[] t) throws IOException {
-		super(t);
+	public Ex3009() throws IOException {
+		super(testo);
 		// TODO Auto-generated constructor stub
 	}
+
+	public static String[] testo = {"Dati 3 vettori di caratteri, quali hanno solo cifre?","prodotto vettore di reali",
+		"scrivi numero da vettore di interi","Conta elementi distinti in un vettore",
+		"Verifica se in un vettore di interi c'è uno 0","ricerca binaria su vettore di caratteri :D"};
+	private static int sizeV = 3;
 
 }
