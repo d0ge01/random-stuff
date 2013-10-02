28caccaab28f73ff9c9f62a678ccd190db07a140
d0ge01 <not.d0ge@anche.no>
2013-10-02 21:04:40 +0200

    added *

--- NComplex/src/it/d0ge01/ncomplex/Main.java
+++ NComplex/src/it/d0ge01/ncomplex/Main.java
@@ -14,17 +14,39 @@ public class Main {
 	
 	private static void menu() throws IOException {
 		String output = "";
+		int[] n1 = new int[2];
+		int[] n2 = new int[2];
 		int[] r = new int[2];
 		
 		Parser uno = new Parser();
 		while ( true ) {
-			System.out.println("inserisci un numero reale: ( N ( cos 50 isin 50 ) oppure n + mi )");
-			output = Utils.inputString();
+			System.out.println("inserisci il primo numero reale: n + mi )");
+			output = Utils.inputString();	
+			n1 = uno.parser(output);
 			
-			r = uno.parser(output);
-			for ( int i = 0 ; i < 2 ; i++ ) {
-				System.out.println(r[i]);
-			}
+			System.out.println("inserisci il secondo numero reale: n + mi )");
+			output = Utils.inputString();
+			n2 = uno.parser(output);
+			
+			System.out.println(complex2String(n1) + " * " + complex2String(n2) + " = ");
+			
+			r = prodotto(n1, n2);
+			System.out.println(complex2String(r));
 		}
+	}
+	
+	private static String complex2String ( int[] v ) {
+		if ( v.length > 1 )
+			return " " + v[0] + " + " + v[1] + "i ";
+		return "";
+	}
+	
+	private static int[] prodotto(int[] v1, int[] v2) {
+		int[] ritorno = new int[2];
+		
+		ritorno[0] = ( v1[0] * v2[0] - v1[1] * v1[1]);
+		ritorno[1] = ( v1[1] * v2[0] + v1[0] * v2[1]);
+		
+		return ritorno;
 	}
 }
--- NComplex/src/it/d0ge01/ncomplex/parser/Parser.java
+++ NComplex/src/it/d0ge01/ncomplex/parser/Parser.java
@@ -14,13 +14,11 @@ public class Parser {
 		Matcher m;
 		
 		if ( buff.matches("\\d+ \\D \\d+i")  ) {
-			System.out.println("1 caso :D");
 			p = Pattern.compile("(\\d+) \\D{1} (\\d+)i");
 			m = p.matcher(buff);
 			c = 0;
 			
 			while(m.find()) {
-				System.out.println("found a " + m.group());
 				ret[0] = Integer.parseInt(m.group(1));
 				ret[1] = Integer.parseInt(m.group(2));
 			}
